09ff5acd96570895a9fec8f9961d3baf46ac78ed
Laptop-Nuub <94393749+Laptop-Nuub@users.noreply.github.com>
2021-11-23 08:14:13 +0530

    Fixed Fernet

--- Backend_With_CSV.py
+++ Backend_With_CSV.py
@@ -1,10 +1,8 @@
 import csv
 import hashlib
 from os import times
-import os
 import time
 from datetime import datetime, date
-from cryptography import fernet
 from cryptography.fernet import Fernet
 
 
@@ -17,10 +15,12 @@ def create_account() -> tuple:
     # Checking if there already is a record with the unique ID in the database (CSV file)
     user_data = csv.reader(open(r"./user.csv"))
     for row in user_data:
-        if row[0] == uid:
-            for detail in row:
-                data.append(detail)
-            break
+        for details in row:
+            print(details)
+            if row[0] == uid:
+                for detail in row:
+                    data.append(detail)
+                break
     # If no record with the unique ID in the database exists, asks for the data and adds it to the database.
     if len(data) == 0:
         data.append(uid)
@@ -29,7 +29,7 @@ def create_account() -> tuple:
         data.append(input(" Date of Birth: "))
         data.append(int(input("Phone Number: ")))
         data.append(input("Email Address:"))
-        csv.writer(open(r"./user.csv", "a")).writerows([data])
+        csv.writer(open(r"./user.csv", "a",newline="\n")).writerows([data])
     print("Your record already exists in the database")
     print("----------------------------------------------------------------")
     print("Unique ID: %s\nName: %s\nAddress: %s\nDate of Birth: %s\nPhone Number: %s\nEmail Address: %s\n" % (data[0],
@@ -41,7 +41,7 @@ def create_account() -> tuple:
     # Account Signing Form
     account_types = {1: "Savings Account",
                      2: "Current Account",
-                     3: "Corporate Account"}
+                     3: "Bussiness Account"}
     print(account_types)
     account_type = account_types[int(input("Account Type: "))]
     print(account_type)
@@ -61,10 +61,12 @@ def create_account() -> tuple:
         account_number = account_number+(user_count)
         timestamp = time.time()
         with open(r"./account_details.csv","a",encoding="utf-8",newline="\n") as account_database:
-            details = [account_number,uid, first_amount]
+            details = [account_number,uid,timestamp]
             csv.writer(account_database).writerow(details)
         print("Account created successfully")
+        # Salts the account number with the timestamp.
         hash_text = account_number.join(str(timestamp))
+        # Hashes the resulting string.
         result = hashlib.sha256(hash_text.encode()).hexdigest()
         passbook_name = result+".csv"
         with open(r"./Passbooks/"+passbook_name,"w",newline="\n") as passbook:
@@ -76,26 +78,68 @@ def create_account() -> tuple:
             csv.writer(passbook).writerow(fields)
             csv.writer(passbook).writerow(details)
             print("Passbook created successfully")
-        password = input("Enter a atleast 8 characters password: ") # Check regex pattern later on.
-        password = hashlib.sha256(password.encode()).hexdigest()
+        # Encrypt the passbook.
         key = Fernet.generate_key()
         with open(r"./Passbooks/"+passbook_name, 'rb') as unencrypted:
             _file = unencrypted.read()
             encrypted = Fernet(key).encrypt(_file)
         with open(r"./Passbooks/"+passbook_name, 'wb') as encrypted_file:
             encrypted_file.write(encrypted)
-            print("Your key to access your account passbook is\n:",key)
+            print("Your key to access your account passbook is:\n",key)
             if input("Do you want to save this key? Enter y for yes:") == 'y':
-                directory = os.path.join(account_number+"_key.key")
+                directory = "./Keys/"+account_number+"_key.key"
                 open(directory, 'wb').write(key)
                 print("Your key is saved at",directory)
 
                 
 def login() -> tuple:
     uid = input("Enter Unique ID: ")
-    password = input("Enter Password: ")
-    
-
+    if len(uid) !=16:
+        print("Invalid Unique ID")
+        return None
+    with open(r"./account_details.csv",newline="\n") as database:
+        list_of_account = csv.DictReader(database)
+        user_accounts = list()
+        for details in list_of_account:
+            if details['user unique id'] == uid:
+                print("--------------------------------------------------------")
+                print("Account number:", details['account number'])
+                account_type = details["account number"]
+                if "SA" in account_type:
+                    account_type = "Savings"
+                elif "CA" in account_type:
+                    account_type = "Current"
+                else:
+                    account_type = "Bussiness"
+                print("Account type:", account_type)
+                user_accounts.append(details)
+                account_number = input("Enter the account number you want to login in: ")
+                for user in user_accounts:
+                    if user['account number'] == account_number:
+                        timestamp = user['timestamp']
+                        # Salts the account number with the timestamp.
+                        hash_text = account_number.join(str(timestamp))
+                        print(timestamp)
+                        # Hashes the resulting string.
+                        result = hashlib.sha256(hash_text.encode()).hexdigest()
+                        passbook_name = result+".csv"
+                        passbook_location = r"./Passbooks/"+passbook_name
+                        key = Fernet(input("Enter the account key:"))
+                        with open(passbook_location,"rb") as account:
+                            print(account.read())
+                        with open(passbook_location,'rb') as file:
+                            encrypted = file.read()
+                        decrypted = key.decrypt(encrypted)
+                        with open(passbook_location,"wb") as file:
+                            file.write(decrypted)
+                        with open(passbook_location) as account:
+                            reader = csv.reader(account)
+                            for row in reader:
+                                print(row)
+        if len(user_accounts) == 0:
+            print("No account linked to your unique ID found.")
+            if input("Do you want to create an account?\nEnter y for yes:") == 'y':
+                create_account()    
 if __name__ == '__main__':
     print("User: 1\n"
           "Admin: 2")
